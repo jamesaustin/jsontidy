9a8c830f924c26f824683ba2a49d9ab413dee263
James Austin <jamesaustin@mac.com>
2019-07-25 12:10:54 +0100

    Tidy with: black -l 120

--- jsontidy.py
+++ jsontidy.py
@@ -5,21 +5,17 @@ from re import compile as re_compile
 
 # Taken from https://github.com/python/cpython/blob/master/Lib/json/encoder.py
 ESCAPE_RE = re_compile(r'[\x00-\x1f\\"\b\f\n\r\t]')
-ESCAPE_DICT = {
-    '\\': '\\\\',
-    '"': '\\"',
-    '\b': '\\b',
-    '\f': '\\f',
-    '\n': '\\n',
-    '\r': '\\r',
-    '\t': '\\t',
-}
+ESCAPE_DICT = {"\\": "\\\\", '"': '\\"', "\b": "\\b", "\f": "\\f", "\n": "\\n", "\r": "\\r", "\t": "\\t"}
 for i in range(0x20):
-    ESCAPE_DICT.setdefault(chr(i), '\\u{0:04x}'.format(i))
+    ESCAPE_DICT.setdefault(chr(i), "\\u{0:04x}".format(i))
+
+
 def encode_basestring(s):
     def replace(match):
         return ESCAPE_DICT[match.group(0)]
+
     return '"' + ESCAPE_RE.sub(replace, s) + '"'
+
 
 # Configure the output rules.
 EMPTY = True
@@ -29,29 +25,35 @@ LIST_OF_BOOLS = True
 LIST_OF_LISTS_OF = False
 DICT_OF_NUMBERS = True
 DICT_OF_BOOLS = True
-DICT_MAX_ELEMENTS = 5 # allows RGBA or XYZW or RGBLr style dicts
-FLOAT_FORMAT = '{:g}' # limits precision to ~6dp
+DICT_MAX_ELEMENTS = 5  # allows RGBA or XYZW or RGBLr style dicts
+FLOAT_FORMAT = "{:g}"  # limits precision to ~6dp
+
 
 def dump(data, stream, indent):
     def tab(d):
-        return ' ' * (indent * d)
+        return " " * (indent * d)
+
     def config_list(o, d):
-        if (LIST_OF_NUMBERS and all(isinstance(x, (int, float)) and not isinstance(x, bool) for x in o)) or \
-            (LIST_OF_BOOLS and all(isinstance(x, bool) for x in o)) or \
-            (LIST_OF_LISTS_OF and all(isinstance(x, list) and can_compact_list(x) for x in o)) or \
-            (LIST_SINGLES and len(o) == 1) or \
-            (EMPTY and len(o) == 0):
-            return ('[', ', ', ']', False, d)
+        if (
+            (LIST_OF_NUMBERS and all(isinstance(x, (int, float)) and not isinstance(x, bool) for x in o))
+            or (LIST_OF_BOOLS and all(isinstance(x, bool) for x in o))
+            or (LIST_OF_LISTS_OF and all(isinstance(x, list) and can_compact_list(x) for x in o))
+            or (LIST_SINGLES and len(o) == 1)
+            or (EMPTY and len(o) == 0)
+        ):
+            return ("[", ", ", "]", False, d)
         else:
-            return ('[\n', ',\n', '\n' + tab(d) + ']', True, d+1)
+            return ("[\n", ",\n", "\n" + tab(d) + "]", True, d + 1)
+
     def config_dict(o, d):
-        if (DICT_MAX_ELEMENTS >= len(o)) and (\
-            (DICT_OF_NUMBERS and all(isinstance(x, (int, float)) and not isinstance(x, bool) for x in o)) or \
-            (DICT_OF_BOOLS and all(isinstance(x, bool) for x in o)) or \
-            (EMPTY and len(o) == 0)):
-            return ('{', '"{}": ', ', ', '}')
+        if (DICT_MAX_ELEMENTS >= len(o)) and (
+            (DICT_OF_NUMBERS and all(isinstance(x, (int, float)) and not isinstance(x, bool) for x in o))
+            or (DICT_OF_BOOLS and all(isinstance(x, bool) for x in o))
+            or (EMPTY and len(o) == 0)
+        ):
+            return ("{", '"{}": ', ", ", "}")
         else:
-            return ('{\n', tab(d+1) + '"{}": ', ',\n', '\n' + tab(d) + '}')
+            return ("{\n", tab(d + 1) + '"{}": ', ",\n", "\n" + tab(d) + "}")
 
     def encode(o, requires_indent=True, depth=0):
         if requires_indent:
@@ -70,16 +72,16 @@ def dump(data, stream, indent):
             yield start
             for n, (k, v) in enumerate(o.items()):
                 yield key.format(k)
-                yield from encode(v, False, depth+1)
+                yield from encode(v, False, depth + 1)
                 if n < len(o) - 1:
                     yield comma
             yield end
         elif o is True:
-            yield 'true'
+            yield "true"
         elif o is False:
-            yield 'false'
+            yield "false"
         elif o is None:
-            yield 'null'
+            yield "null"
         elif isinstance(o, int):
             yield str(o)
         elif isinstance(o, float):
@@ -87,18 +89,19 @@ def dump(data, stream, indent):
         elif isinstance(o, str):
             yield encode_basestring(o)
         else:
-            print(f'ERROR: {o}')
+            print(f"ERROR: {o}")
 
     stream.writelines(encode(data))
-    stream.write('\n')
+    stream.write("\n")
 
-parser = ArgumentParser(description='Tidy JSON files in-place.')
-parser.add_argument('files', metavar='FILE', type=str, nargs='+', help='file to tidy')
-parser.add_argument('-i', '--indent', type=int, default=4, help='number of spaces per depth')
+
+parser = ArgumentParser(description="Tidy JSON files in-place.")
+parser.add_argument("files", metavar="FILE", type=str, nargs="+", help="file to tidy")
+parser.add_argument("-i", "--indent", type=int, default=4, help="number of spaces per depth")
 args = parser.parse_args()
 
 for filename in args.files:
     with open(filename) as f:
         data = json_load(f)
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         dump(data, f, args.indent)
